8303c34eede12c42d7ff8e5100056a4ac645c1a4
Harald Hultin <harald.hultin@outlook.com>
2023-11-28 15:33:06 +0100

    added fibonacci sequence and tests

---
+++ common_practices/exercises/fibonacci.py
@@ -0,0 +1,25 @@
+def fibonacci(length, start_at_1=False):
+    """returns a fibonacci sequence of specified length, starting at 0 or 1"""
+    if start_at_1:
+        start = [1,1]
+    else:
+        start = [0,1]
+    for index in range(length):
+        if index == 0:
+            sequence = [start[0]]
+        else:
+            if index == 1:
+                element = start[1]
+            else:
+                element = sequence[index-1] + sequence[index-2]
+            sequence = sequence+[element]
+    return sequence
+
+assert fibonacci(1) == [0]
+
+assert fibonacci(2) == [0,1]
+
+assert fibonacci(3) == [0,1,1]
+
+assert fibonacci(3, True) == [1,1,2]
+
